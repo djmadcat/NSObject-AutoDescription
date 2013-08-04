dae705f9c7b8cf8c6b47ca00b503c837f715def5
Alexey Aleshkov <djmadcat@gmail.com>
2013-08-05 00:02:47 +0400

    Code style: tabs vs spaces

--- Example/NSObject-AutoDescription-iOS/NAAppDelegate_iOS.m
+++ Example/NSObject-AutoDescription-iOS/NAAppDelegate_iOS.m
@@ -15,14 +15,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+	// Override point for customization after application launch.
+	self.window.backgroundColor = [UIColor whiteColor];
+	[self.window makeKeyAndVisible];
 
 	[NAMain main];
 
-    return YES;
+	return YES;
 }
 
 @end
--- Example/NSObject-AutoDescription-iOS/main.m
+++ Example/NSObject-AutoDescription-iOS/main.m
@@ -14,6 +14,6 @@
 int main(int argc, char *argv[])
 {
 	@autoreleasepool {
-	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([NAAppDelegate_iOS class]));
+		return UIApplicationMain(argc, argv, nil, NSStringFromClass([NAAppDelegate_iOS class]));
 	}
 }
